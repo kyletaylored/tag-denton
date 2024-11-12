7a91e040fa68f380996fbb254ca2fbbdca8f5f74
Kyle Taylor <kyle.taylor@datadoghq.com>
2024-11-12 13:51:09 -0600

    Fix dotenv

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ require 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-// Load .env variables
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// Check if the .env file exists and load it for local development
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
 
 require_once __DIR__ . '/src/Routes/routes.php';
 
